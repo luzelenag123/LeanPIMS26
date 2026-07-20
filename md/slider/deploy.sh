b01c844d193193367a7473a53ff7b5f963356cb6
Eric Klavins <klavins@uw.edu>
2026-07-20 08:29:24 -0700

    moving docs dir

--- md/slider/deploy.sh
+++ md/slider/deploy.sh
@@ -1,29 +1,37 @@
-mkdir -p dist
-mkdir -p  dist/vendor
-cp  *.html *.js *.css *.json dist
-cp -r src dist
+#!/usr/bin/env bash
+set -e
 
-mkdir -p  dist/vendor/github-markdown-css
-cp node_modules/github-markdown-css/github-markdown.css dist/vendor/github-markdown-css
+cd "$(dirname "$0")"
+DEST=../../docs
 
-mkdir -p  dist/vendor/js-cookie/
-mkdir -p  dist/vendor/js-cookie/src/
-cp node_modules/js-cookie/src/js.cookie.js dist/vendor/js-cookie/src
+mkdir -p $DEST
+mkdir -p $DEST/vendor
+cp  *.html *.js *.css *.json $DEST
+cp -r src $DEST
 
-mkdir -p  dist/vendor/showdown
-mkdir -p  dist/vendor/showdown/dist
-cp node_modules/showdown/dist/showdown.min.js dist/vendor/showdown/dist
+mkdir -p  $DEST/vendor/github-markdown-css
+cp node_modules/github-markdown-css/github-markdown.css $DEST/vendor/github-markdown-css
 
-mkdir -p  dist/vendor/showdown-katex
-mkdir -p  dist/vendor/showdown-katex/dist
-cp node_modules/showdown-katex/dist/showdown-katex.min.js dist/vendor/showdown-katex/dist
+mkdir -p  $DEST/vendor/js-cookie/
+mkdir -p  $DEST/vendor/js-cookie/src/
+cp node_modules/js-cookie/src/js.cookie.js $DEST/vendor/js-cookie/src
 
-mkdir -p  dist/vendor/react
-mkdir -p  dist/vendor/react/umd
-cp  node_modules/react/umd/react.production.min.js dist/vendor/react/umd
+mkdir -p  $DEST/vendor/showdown
+mkdir -p  $DEST/vendor/showdown/dist
+cp node_modules/showdown/dist/showdown.min.js $DEST/vendor/showdown/dist
 
-mkdir -p  dist/vendor/react-dom
-mkdir -p  dist/vendor/react-dom/umd
-cp node_modules/react-dom/umd/react-dom.production.min.js dist/vendor/react-dom/umd
+mkdir -p  $DEST/vendor/showdown-katex
+mkdir -p  $DEST/vendor/showdown-katex/dist
+cp node_modules/showdown-katex/dist/showdown-katex.min.js $DEST/vendor/showdown-katex/dist
 
-cp -r img dist
+mkdir -p  $DEST/vendor/react
+mkdir -p  $DEST/vendor/react/umd
+cp  node_modules/react/umd/react.production.min.js $DEST/vendor/react/umd
+
+mkdir -p  $DEST/vendor/react-dom
+mkdir -p  $DEST/vendor/react-dom/umd
+cp node_modules/react-dom/umd/react-dom.production.min.js $DEST/vendor/react-dom/umd
+
+cp -r img $DEST
+
+touch $DEST/.nojekyll
